d7f07b1e59d6f91a1c53a56ba778052e3a121856
miccls <martincsvardsjo@gmail.com>
2024-12-09 19:22:52 +0100

    Updated day 2

--- 2024/Day2/solution.py
+++ 2024/Day2/solution.py
@@ -1,45 +1,41 @@
 import numpy as np
 
+def sign(x):
+    if x:
+        return x/abs(x)
+    return 0
+
 def get_report(path):
     with open(path, "r+") as f:
         lines = f.readlines()
-    reps = {}
-    for line in lines:
-        line = [int(s) for s in line.split()]
-        if len(line) in reps:
-            reps[len(line)] = np.concatenate((reps[len(line)], np.array([line])))
-        else:
-            reps[len(line)] = np.array([line])
-
+    reps = [list(map(int, line.split())) for line in lines]
     return reps
 
 def check_sub_rep(lst, i): 
-    sgn = lambda a: 0 if a == 0 else a/abs(a)
-    tmp = np.concatenate((lst[:i], lst[i+1:]))
+    tmp = np.concatenate((lst[:i], lst[i+1:])) if i != -1 else lst
     diffs = [tmp[j+1] - tmp[j] for j in range(len(tmp)-1)]
-    over_all_sign = -1 if sum([sgn(a) for a in diffs]) < 0 else 1
-    if all([sgn(a) == over_all_sign and abs(a) < 4 for a in diffs]):
+    majority_sign = -1 if sum([sign(a) for a in diffs]) < 0 else 1
+    if all([sign(a) == majority_sign and abs(a) < 4 for a in diffs]):
         return True
     return False
 
-def check_safe(lst):
+def check_safe(lst, part_two):
     # Loop over list and see how many is unsafe
-   
     for i in range(len(lst)):
-       if check_sub_rep(lst, i):
+       if check_sub_rep(lst, i) and part_two:
            return True        
     # Check whole array
     if check_sub_rep(lst, -1):
         return True
+    
     return False
 
+reps = get_report("2024/Day2/report.txt")
 
-
-reps = get_report("Day2/report.txt")
+part_two = False
 
 # Split into two parts
 total_safe = 0
-for k, rep in reps.items():
-    safe = sum([check_safe(d) for d in rep])
-    total_safe += safe
+for rep in reps:
+    total_safe += check_safe(rep,part_two)
 print(total_safe)
